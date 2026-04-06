dbd92c06fbd314297799ebe3de15bcf72f1dc898
konni pavitra <konnipavitra@gmail.com>
2026-04-06 17:59:15 +0530

    hlw1

--- src/com/patterns/Hallow.java
+++ src/com/patterns/Hallow.java
@@ -3,8 +3,7 @@ package com.patterns;
 public class Hallow {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.err.println("dont use git......");
 	}
 
 }
